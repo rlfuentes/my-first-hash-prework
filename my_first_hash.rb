d7b8ec811cf8fdf71accdb8f9902aded1c003645
Robert <r_lee94@yahoo.com>
2019-02-18 01:18:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -34,13 +34,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+"muskets" => "2"
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
-
+"gun powder" => "4"
+ 
   # return the shipping_manifest hash below
 
 end
